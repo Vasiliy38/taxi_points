e3f0dfe980db923d68b32b40909483c362752893
Vasiliy38 <109076846+Vasiliy38@users.noreply.github.com>
2023-05-22 20:35:16 +1100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,47 @@
+function init() {
+
+    fetch('points.json')
+    .then(obj => obj.json())
+    .then(obj => {
+
+
+            const searchControls = new ymaps.control.SearchControl({
+                options: {
+                    float: 'right',
+                    noPlacemark: true
+                }
+            })
+
+            const myMap = new ymaps.Map("map", {
+                center: [55.76, 37.64],
+                zoom: 7,
+                controls: [searchControls]
+            })
+
+            const removeControls = [
+                'geolocationControl',
+                'trafficControl',
+                'fullscreenControl',
+                'zoomControl', 'rulerControl',
+                'typeSelector'
+            ]
+
+            const clearTheMap = myMap => {
+                removeControls
+                    .forEach(controls => myMap.controls.remove(controls))
+            }
+
+            clearTheMap(myMap)
+
+            const objectManager = new ymaps.ObjectManager({
+                clusterize: true,
+                clusterIconLayout: "default#pieChart"
+            })
+
+
+            objectManager.add(obj)
+            myMap.geoObjects.add(objectManager)
+
+    })
+}
+ymaps.ready(init)
